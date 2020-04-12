7410dbaf4cac8b856c76fa51599f1bafde711ee7
Darkfang18 <61396785+Darkfang18@users.noreply.github.com>
2020-04-11 17:45:47 -0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,12 @@
+#I stored a list of citrus fruit in a array
+citrus_fruit=["lime","lemon","orange","tangerine","grape furit"]
+#printed a blank line
+print()
+#printed the list of fruits
+print(citrus_fruit)
+#I accessed list by index from 2nd index to not including the element in the fourth index
+print(citrus_fruit[2:4])
+#I accessed all the list elements from second index
+print (citrus_fruit[2:])
+#I accessed all the list elements from 0th index to not including the element in the fourth index
+print (citrus_fruit[:4])
