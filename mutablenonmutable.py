814fc1926898ee0dec43eb9962b9abe7dd97601e
Darkfang18 <61396785+Darkfang18@users.noreply.github.com>
2020-04-11 17:15:37 -0700

    Create mutablenonmutable.py

---
+++ mutablenonmutable.py
@@ -0,0 +1,22 @@
+#I Declared and assign value to the list
+
+my_favorite_desert= ["brownie","icecream","cake"]
+
+
+#I printed the list
+
+print(my_favorite_desert)
+#I emptied the list
+my_favorite_desert=[]
+
+#I printed the empty list   
+print(my_favorite_desert)
+#I added a item to the list
+my_favorite_desert+= ["brownie","icecream","cake"]
+#I printed the list
+print(my_favorite_desert)
+
+#Addding one more desert
+my_favorite_desert+=["donuts"]
+#I printed the updated list
+print(my_favorite_desert)
