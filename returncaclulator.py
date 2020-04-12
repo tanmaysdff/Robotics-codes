2d9e8d05e73779e36c1fda784511391d9862efa2
Darkfang18 <61396785+Darkfang18@users.noreply.github.com>
2020-04-11 17:18:46 -0700

    Update returncaclulator.py

--- returncaclulator.py
+++ returncaclulator.py
@@ -1,19 +1,25 @@
-def addNumbers(num1,num2):
-    answer_add = num1+num2
-    return answer_add
-     
-def subtractNumbers(num1,num2):
+
+       def subtractNumbers(num1,num2,display=False):
     answer_subtract = num1-num2
-    return answer_subtract
+    if display:
+        print("First number-second number is:",answer_subtract)
+    else:
+         return answer_subtract
 
 
-def DivideNumbers(num1,num2):
+def DivideNumbers(num1,num2,display=False):
     answer_Divide = num1/num2
-    return answer_Divide
+    if display:
+        print("First number/second number is:",answer_Divide)
+    else:
+        return answer_Divide
 
-def MultiplyNumbers(num1,num2):
+def MultiplyNumbers(num1,num2,display=False):
     answer_Multiply = num1*num2
-    return answer_Multiply
+    if display:  
+        print("First number*second number is:",answer_Multiply)
+    else:
+        return answer_Multiply
 
 print ("Hi I am a calculator")
 while True:
@@ -21,22 +27,22 @@ while True:
     if operation == "Add":
         a=float(input("Enter number."))
         b=float(input("Enter another number."))
-        global_add = addNumbers(a,b)
+        global_add = addNumbers(a,b,True)
         print(global_add)
     elif operation == "Subtract":
         a=float(input("Enter number."))
         b=float(input("Enter another number."))
-        global_subtract = subtractNumbers(a,b)
+        global_subtract = subtractNumbers(a,b,True)
         print(global_subtract)
     elif operation == "Divide":
         a=float(input("Enter number."))
         b=float(input("Enter another number."))
-        global_Divide = DivideNumbers(a,b)
+        global_Divide = DivideNumbers(a,b,True)
         print(global_Divide)
     elif operation == "Multiply":
         a=float(input("Enter number."))
         b=float(input("Enter another number."))
-        global_Multiply = MultiplyNumbers(a,b)
+        global_Multiply = MultiplyNumbers(a,b,True)
         print(global_Multiply)
     elif operation== "quit":
         break
