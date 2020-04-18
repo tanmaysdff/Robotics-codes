65a6440d852f7191ed02aa25b669d945a8677f7a
Darkfang18 <61396785+Darkfang18@users.noreply.github.com>
2020-04-18 16:59:33 -0700

    Create appenddelete.py

---
+++ appenddelete.py
@@ -0,0 +1,7 @@
+my_favorite_desserts=['brownie','cake','chocolate']
+# I forgot to add a dessert
+my_favorite_desserts.append('donuts')
+print(my_favorite_desserts)
+#cake is not my favorite dessert anymore
+my_favorite_desserts.remove(‘cake’)
+print(my_favorite_desserts)
