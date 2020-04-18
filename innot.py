18a4ce948ed2a75081f7e37fedfbc98efb41edc2
Darkfang18 <61396785+Darkfang18@users.noreply.github.com>
2020-04-18 11:37:05 -0700

    Create innot.py

---
+++ innot.py
@@ -0,0 +1,11 @@
+citrus_fruits = ['Orange', 'Lemon', 'Grapefruit', 'Pomelo', 'Lime']
+
+if 'Apple' in citrus_fruits:
+	print('Apple is a citrus fruit')
+else:
+	print('Apple is not a citrus fruit')
+
+if 'Donuts' not in citrus_fruits:
+	print('Donuts is not a citrus fruit')
+else:
+	print('Donuts is a citrus fruit')
