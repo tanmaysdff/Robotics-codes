3df1e1395594c827ab01dbf25e7c77fd37493d78
Darkfang18 <61396785+Darkfang18@users.noreply.github.com>
2020-04-18 17:18:02 -0700

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,10 @@
+#I Used parentheses () to hold items, instead of the brackets[].
+#Tuples are immutable.
+
+
+rgb_colors = ('red', 'green', 'blue')
+
+
+#I tried to modify a Tuple using below statement and failed?
+
+rgb_colors[0] = 'Orange'
