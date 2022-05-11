b599255f742bced119eb0c47c988ae933f1cea20
tanmaysdff <61396785+tanmaysdff@users.noreply.github.com>
2022-05-10 22:23:03 -0700

    Update Calculator(Unfinished).java

--- Calculator(Unfinished).java
+++ Calculator(Unfinished).java
@@ -4,42 +4,81 @@ class Solution {
   }
 
 public static int subtract(int numberone, int numbertwo) {
-    return numberone - numbertwo;
-
-}
-  
+    return numberone - numbertwo; 
+    }
+  public static int divide(int numberone, int numbertwo) {
+    return numberone/numbertwo; 
+    }
+    public static int multiply(int numberone, int numbertwo) {
+    return numberone*numbertwo; 
+    }  
   public static void main(String[] args) {
     System.out.println ("Please select operation");
     String operation = System.console().readLine();
     String minus_sub = "-";
-    if (operation == minus_sub){
-    System.out.println("This is working");
-    }
-    /*
+
+    if (operation.equals(minus_sub)) {
+    System.out.println("This is a subtraction operation");
     System.out.println("Please enter the first number");
     String number_one = System.console().readLine();
     System.out.println("Please enter the second number");
     String number_two = System.console().readLine();
     
     System.out.println("The answer is:");
-
     int result = subtract  (Integer.parseInt(number_one), Integer.parseInt(number_two));
     
     System.out.println(result);
-    */
-}
-    
-    /* System.out.println("Please enter the first number");
-    String numberone = System.console().readLine();
+      }
+
+    if (operation.equals("+")) {
+    System.out.println("This is an addition operation");
+    System.out.println("Please enter the first number");
+    String number_one = System.console().readLine();
     System.out.println("Please enter the second number");
-    String numbertwo = System.console().readLine();
+    String number_two = System.console().readLine();
     
     System.out.println("The answer is:");
-
-    int result = add  (Integer.parseInt(numberone), Integer.parseInt(numbertwo));
-    System.out.println(result);
-  
-   
-*/
+    int result = add (Integer.parseInt(number_one), Integer.parseInt(number_two));
     
-  }
+    System.out.println(result);
+      }
+
+       if (operation.equals("/")) {
+    System.out.println("This is a division operation");
+    System.out.println("Please enter the first number");
+    String number_one = System.console().readLine();
+    System.out.println("Please enter the second number");
+    String number_two = System.console().readLine();
+    
+    System.out.println("The answer is:");
+    int result = divide(Integer.parseInt(number_one), Integer.parseInt(number_two));
+    
+    System.out.println(result);
+      
+    }
+     if (operation.equals("*")) {
+    System.out.println("This is a multiplication operation");
+    System.out.println("Please enter the first number");
+    String number_one = System.console().readLine();
+    System.out.println("Please enter the second number");
+    String number_two = System.console().readLine();
+    
+    System.out.println("The answer is:");
+    int result = multiply(Integer.parseInt(number_one), Integer.parseInt(number_two));
+    
+    System.out.println(result);
+      
+      
+     }
+     System.out.println("Would you like to continue? y or n");
+     String desision = System.console().readLine();
+      
+      
+      if (desision.equals("n")) {
+        System.exit(0);
+      }
+      if (desision.equals("y")) {
+      
+      }
+  } 
+}  
